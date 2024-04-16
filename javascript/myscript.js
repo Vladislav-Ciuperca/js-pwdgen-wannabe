85c93b5bc0071b6ddb9c0c506e3eed7beba7d489
Vladislav-Ciuperca <ciuperca37@gmail.com>
2024-04-16 21:14:41 +0200

    commit 2

---
+++ javascript/myscript.js
@@ -0,0 +1,7 @@
+alert = "inserire nome"
+let nome = prompt("inserisci il tuo nome")
+let cognome = prompt("inserisci il tuo cognome")
+let colore = prompt("inserisci il tuo colore preferito :3")
+
+document.getElementById("password").innerHTML = nome + cognome + colore
+
